f228b02090c6e9e4ae9518273827a0724cdbe206
Michael P. Starkweather <mpstark@gmail.com>
2018-12-28 03:41:47 -0600

    Change formula of salvage

--- StarkSalvage/Patches.cs
+++ StarkSalvage/Patches.cs
@@ -34,23 +34,34 @@ namespace StarkSalvage
                 var mechDef = unitResult.mech;
                 var pilotDef = unitResult.pilot;
 
-                // if the mech wasn't destroyed or the pilot wasn't killed
-                // then we don't get to salvage
+                // if the mech wasn't destroyed or the pilot wasn't killed then we don't get to salvage
                 // thanks to morph's AdjustedMechSalvage for pointing out the critical component stuff
                 if (!pilotDef.IsIncapacitated && !mechDef.IsDestroyed && !mechDef.Inventory.Any(x => x.Def != null && x.Def.CriticalComponent && x.DamageLevel == ComponentDamageLevel.Destroyed))
                     continue;
+                
+                float bits = 0;
 
-                float numParts = 1;
+                // CT is worth 1/2 of the salvage
                 if (!mechDef.IsLocationDestroyed(ChassisLocations.CenterTorso))
-                {
-                    // add limb value for each intact limb
-                    foreach (var limbLocation in LIMB_LOCATIONS)
-                        if (!mechDef.IsLocationDestroyed(limbLocation))
-                            numParts += (maxMechParts - 1) / LIMB_LOCATIONS.Length;
-                }
+                    bits += maxMechParts / 2;
+                
+                // limbs combined are worth the other 1/2 of the salvage, so 1/8 each
+                foreach (var limbLocation in LIMB_LOCATIONS)
+                    if (!mechDef.IsLocationDestroyed(limbLocation))
+                        bits += maxMechParts / 8;
 
-                Main.HBSLog.Log($"CreateAndAddMechPart {mechDef.Description.Id} {(int)Math.Round(numParts)}");
-                instTrav.Method("CreateAndAddMechPart", simGame.Constants, mechDef, (int)Math.Round(numParts), finalPotentialSalvage).GetValue();
+                // just legs 2/8 + 4/8 = 6/8 salvage * 5 mechPieces = 3.75 ~ 4
+                // both shoulders + leg = 1/8 + 4/8 = 5/8 * 5 = 3.125 ~ 3
+                // cored with all limbs = 4/8 = 4/8 * 5 = 2.5  ~ 2
+                // cored with 3 limbs 3/8 * 5
+
+                // round .5 down by subtracting a little
+                int mechParts = (int)Math.Round(bits - 0.05);
+
+                Main.HBSLog.Log($"{mechDef.Description.Id} {mechParts}");
+
+                if (mechParts > 0)
+                    instTrav.Method("CreateAndAddMechPart", simGame.Constants, mechDef, mechParts, finalPotentialSalvage).GetValue();
             }
         }
     }
