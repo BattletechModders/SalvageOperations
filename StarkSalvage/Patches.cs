d9be15efe917157231cc0c0b235a30723bbaa724
Michael P. Starkweather <mpstark@gmail.com>
2019-01-04 21:26:25 -0600

    Add additional event descriptions for different situations

--- StarkSalvage/Patches.cs
+++ StarkSalvage/Patches.cs
@@ -364,6 +364,15 @@ namespace StarkSalvage
                 }
             };
 
+            var defaultMechPartMax = simGame.Constants.Story.DefaultMechPartMax;
+
+            // setup the event string based on the situation
+            var eventString = "As you board, Yang asks for you to meet him in the 'Mech Bay. When you arrive, you find him grinning in front of a load of unidentifiable scrap.\r\n\r\n\"Commander, we don't have enough salvage from any single 'Mech to build ourselves a new one, but...\" He pauses dramatically. \"...I could cobble together the salvage from a couple related 'Mechs.\"\"\r\n\r\n\"What do you think?\" He grins like a kid in a candy shop. \"Which one should we build?\"";
+            if (mechPieces.Count == 1) // we have only a single option
+                eventString = $"As you board, Yang asks for you to meet him in the 'Mech Bay. When you arrive, you find him grinning in front of a load of unidentifiable scrap.\r\n\r\n\"Commander, we've got enough salvage from the {highestVariant.Description.UIName} to put it together.\" He pauses, rubbing his beard. \"But, we could save it to build another variant, later.\"\"\r\n\r\n\"What do you think?\" He grins like a kid in a candy shop. \"Should we build it?\"";
+            else if (highest >= defaultMechPartMax) // we have enough salvage to build a mech
+                eventString = "As you board, Yang asks for you to meet him in the 'Mech Bay. When you arrive, you find him grinning in front of a load of unidentifiable scrap.\r\n\r\n\"Commander, we've got enough salvage to build a 'Mech out completely, but...\" He pauses dramatically. \"...I could cobble together the salvage from a couple related 'Mechs if you wanted to build something else.\"\"\r\n\r\n\"What do you think?\" He grins like a kid in a candy shop. \"Which one should we build?\"";
+
             // build the event itself
             var eventDef = new SimGameEventDef(
                 SimGameEventDef.EventPublishState.PUBLISHED,
@@ -372,7 +381,7 @@ namespace StarkSalvage
                 new DescriptionDef(
                     "StarkSalvageEventID",
                     $"Playing With Salvage",
-                    "As you board, Yang asks for you to meet him in the 'Mech Bay. When you arrive, you find him grinning in front of a load of unidentifiable scrap.\r\n\r\n\"Commander, we don't have enough salvage from any single 'Mech to build ourselves a new one, but...\" He pauses dramatically. \"...I could cobble together the salvage from a couple related 'Mechs.\"\"\r\n\r\n\"What do you think?\" He grins like a kid in a candy shop. \"Which one should we build?\"",
+                    eventString,
                     "uixTxrSpot_YangWorking.png",
                     0, 0, false, "", "", ""),
                 new RequirementDef { Scope = EventScope.Company },
