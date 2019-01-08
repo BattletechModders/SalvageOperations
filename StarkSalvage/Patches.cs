4f8a9186694731d2d554bfbe2c5e218e03eaca76
Michael P. Starkweather <mpstark@gmail.com>
2019-01-08 05:00:46 -0600

    The patch for showing number of variant pieces cause the game to crash apparently

--- StarkSalvage/Patches.cs
+++ StarkSalvage/Patches.cs
@@ -157,19 +157,19 @@ namespace StarkSalvage
         }
     }
 
-    [HarmonyPatch(typeof(ListElementController_SalvageMechPart_NotListView), "RefreshInfoOnWidget")]
-    public static class ListElementController_SalvageMechPart_NotListView_RefreshInfoOnWidget_Patch
-    {
-        public static void Postfix(ListElementController_SalvageMechPart_NotListView __instance, InventoryItemElement_NotListView theWidget, SimGameState ___simState)
-        {
-            var defaultMechPartMax = ___simState.Constants.Story.DefaultMechPartMax;
-            var thisMechPieces = Main.GetMechPieces(___simState, __instance.mechDef);
-            var allMechPieces = Main.GetAllVariantMechPieces(___simState, __instance.mechDef);
+    //[HarmonyPatch(typeof(ListElementController_SalvageMechPart_NotListView), "RefreshInfoOnWidget")]
+    //public static class ListElementController_SalvageMechPart_NotListView_RefreshInfoOnWidget_Patch
+    //{
+    //    public static void Postfix(ListElementController_SalvageMechPart_NotListView __instance, InventoryItemElement_NotListView theWidget, SimGameState ___simState)
+    //    {
+    //        var defaultMechPartMax = ___simState.Constants.Story.DefaultMechPartMax;
+    //        var thisMechPieces = Main.GetMechPieces(___simState, __instance.mechDef);
+    //        var allMechPieces = Main.GetAllVariantMechPieces(___simState, __instance.mechDef);
 
-            if (allMechPieces > thisMechPieces)
-                theWidget.mechPartsNumbersText.SetText($"{thisMechPieces} ({allMechPieces}) / {defaultMechPartMax}");
-        }
-    }
+    //        if (allMechPieces > thisMechPieces)
+    //            theWidget.mechPartsNumbersText.SetText($"{thisMechPieces} ({allMechPieces}) / {defaultMechPartMax}");
+    //    }
+    //}
 
 
     public static class Main
