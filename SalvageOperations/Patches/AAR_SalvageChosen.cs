09e797187bb2e312c9ebe60505773fa6aa19eed4
Michael P. Starkweather <mpstark@gmail.com>
2019-02-01 20:25:16 -0600

    Add sell value salvage patch, add harmony

---
+++ SalvageOperations/Patches/AAR_SalvageChosen.cs
@@ -0,0 +1,19 @@
+﻿using BattleTech;
+using BattleTech.UI;
+using Harmony;
+
+// ReSharper disable InconsistentNaming
+// ReSharper disable UnusedMember.Global
+
+namespace SalvageOperations.Patches
+{
+    [HarmonyPatch(typeof(AAR_SalvageChosen), "CalculateCurrentSalvageValue")]
+    public static class AAR_SalvageChosen_CalculateCurrentSalvageValue_Patch
+    {
+        public static void Postfix(SimGameState ___simState, ref int __result)
+        {
+            if (___simState != null)
+                __result = (int) (__result * ___simState.Constants.Finances.ShopSellModifier);
+        }
+    }
+}
