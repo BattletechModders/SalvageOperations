7e2336bcdb93dbda950da31730324469d62127ab
gnivler <gnivil@gmail.com>
2019-05-18 00:44:56 -0600

    fix publicized call

--- SalvageOperations/Patches/SimGameInterruptManager.cs
+++ SalvageOperations/Patches/SimGameInterruptManager.cs
@@ -13,10 +13,11 @@ namespace SalvageOperations.Patches
         {
             var mechBayPanel = (MechBayPanel) UIManager.Instance.Find(x => x.IsType(typeof(MechBayPanel)));
             var sim = UnityGameInstance.BattleTechGame.Simulation;
-            if (!mechBayPanel.storageWidget.enabled) return;
+            var storageWidget = Traverse.Create(mechBayPanel).Field("storageWidget").GetValue<MechBayMechStorageWidget>();
+
+            if (!storageWidget.enabled) return;
             try
             {
-                var storageWidget = Traverse.Create(mechBayPanel).Field("storageWidget").GetValue<MechBayMechStorageWidget>();
                 storageWidget.InitInventory(sim.GetAllInventoryMechDefs(), false);
             }
             catch (Exception ex)
