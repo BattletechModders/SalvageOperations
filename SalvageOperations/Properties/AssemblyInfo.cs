d6ba2793dd77d9582ee63d3a041a100cfc2e33b6
gnivler <gnivil@gmail.com>
2019-05-21 18:32:37 -0600

    version bump

--- SalvageOperations/Properties/AssemblyInfo.cs
+++ SalvageOperations/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SalvageOperations")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,4 +31,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.2.*")]
+[assembly: AssemblyVersion("1.3.0.*")]
