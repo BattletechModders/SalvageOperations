6d3f6ac094956de880007e9569d1e38a4e4da0cd
gnivler <gnivil@gmail.com>
2019-06-14 12:32:36 -0600

    bugfix... mod wasn't loading due to bad refactoring

--- SalvageOperations/Patches/MechBayChassisInfoWidget.cs
+++ SalvageOperations/Patches/MechBayChassisInfoWidget.cs
@@ -1,30 +1,28 @@
 using BattleTech;
+using BattleTech.UI;
 using Harmony;
 
 // ReSharper disable InconsistentNaming
 
 namespace SalvageOperations.Patches
 {
-    public class MechBayChassisInfoWidget
+    // some of the patches to enable 99 parts
+    [HarmonyPatch(typeof(MechBayChassisInfoWidget), "SetDescriptions")]
+    public static class MechBayChassisInfoWidget_SetDescriptions_Patch
     {
-        // some of the patches to enable 99 parts
-        [HarmonyPatch(typeof(MechBayChassisInfoWidget), "SetDescriptions")]
-        public static class MechBayChassisInfoWidget_SetDescriptions_Patch
+        public static void Prefix(MechBayChassisInfoWidget __instance, ChassisDef ___selectedChassis, ref int __state)
         {
-            public static void Prefix(MechBayChassisInfoWidget __instance, ChassisDef ___selectedChassis, ref int __state)
+            if (___selectedChassis != null)
             {
-                if (___selectedChassis != null)
-                {
-                    __state = ___selectedChassis.MechPartMax;
-                    ___selectedChassis.MechPartMax = 99;
-                }
+                __state = ___selectedChassis.MechPartMax;
+                ___selectedChassis.MechPartMax = 99;
             }
+        }
 
-            public static void Postfix(MechBayChassisInfoWidget __instance, ChassisDef ___selectedChassis, int __state)
-            {
-                if (___selectedChassis != null)
-                    ___selectedChassis.MechPartMax = __state;
-            }
+        public static void Postfix(MechBayChassisInfoWidget __instance, ChassisDef ___selectedChassis, int __state)
+        {
+            if (___selectedChassis != null)
+                ___selectedChassis.MechPartMax = __state;
         }
     }
 }
