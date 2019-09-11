24f2fa5bb65c9f00b2c674dcb7ca6a5eab616881
Bradley Burcar <btburcar@gmail.com>
2019-09-11 16:10:51 -0400

    1.7-alpha

--- SalvageOperations/Patches/LCE_SalvageMechPart.cs
+++ SalvageOperations/Patches/LCE_SalvageMechPart.cs
@@ -26,7 +26,6 @@ namespace SalvageOperations.Patches
                 theWidget.mechPartsNumbersText.SetText($"{thisMechPieces} ({allMechPieces}) / {defaultMechPartMax}");
             else
                 theWidget.mechPartsNumbersText.SetText($"{thisMechPieces} (R) / {defaultMechPartMax}");
-                
         }
     }
 }
